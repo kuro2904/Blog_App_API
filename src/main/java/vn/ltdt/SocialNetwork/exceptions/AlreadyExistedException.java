6541a597b8e4e88952858e9e044b47c67d3d57e2
kuro2904 <trung2001sgp@gmail.com>
2025-05-25 18:52:38 +0700

    Add AlreadyExistedException

--- src/main/java/vn/ltdt/SocialNetwork/exceptions/AlreadyExistedException.java
+++ src/main/java/vn/ltdt/SocialNetwork/exceptions/AlreadyExistedException.java
@@ -1,12 +1,17 @@
 package vn.ltdt.SocialNetwork.exceptions;
 
-public class AlreadyExistedException extends RuntimeException {
+import lombok.Getter;
 
-    private String dataType;
-    private String field;
-    private String value;
+@Getter
+public class AlreadyExistedException extends RuntimeException {
+    private final String dataType;
+    private final String field;
+    private final String value;
 
     public AlreadyExistedException(String dataType, String field, String value) {
         super(String.format("%s already existed with %s: %s", dataType, field, value));
+        this.dataType = dataType;
+        this.field = field;
+        this.value = value;
     }
 }
