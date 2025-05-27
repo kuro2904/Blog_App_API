316ddd9f321789f42381ebf41242dc6ccb31b9b7
kuro2904 <trung2001sgp@gmail.com>
2025-05-27 07:49:00 +0700

    Fix pagination return with DTO

--- src/main/java/vn/ltdt/SocialNetwork/SocialNetworkApplication.java
+++ src/main/java/vn/ltdt/SocialNetwork/SocialNetworkApplication.java
@@ -3,9 +3,13 @@ package vn.ltdt.SocialNetwork;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class SocialNetworkApplication {
 
 	public static void main(String[] args) {
