e3f45898005ae0056599eb1e09a561eb7095c707
kuro2904 <trung2001sgp@gmail.com>
2025-05-27 07:21:45 +0700

    Remove Blog Request

--- src/main/java/vn/ltdt/SocialNetwork/dtos/blog/BlogRequest.java
+++
@@ -1,9 +0,0 @@
-package vn.ltdt.SocialNetwork.dtos.blog;
-
-import java.util.List;
-
-public record BlogRequest(
-                          String content,
-                          List<String> images
-) {
-}
