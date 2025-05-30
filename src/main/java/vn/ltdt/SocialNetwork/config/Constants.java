724825d1b067b74883a65b1551fa5720a2d5c174
kuro2904 <trung2001sgp@gmail.com>
2025-05-31 03:02:53 +0700

    Update Constants

--- src/main/java/vn/ltdt/SocialNetwork/config/Constants.java
+++ src/main/java/vn/ltdt/SocialNetwork/config/Constants.java
@@ -5,6 +5,6 @@ import java.util.List;
 public class Constants {
     public static final List<String> ALLOWED_CORS_ORIGINS = List.of("http://localhost:3000","http://localhost:5173");
     public static final List<String> ALLOWED_CORS_METHOD = List.of("GET", "POST", "PUT", "DELETE", "OPTIONS");
-    public static final String[] PUBLIC_GET_ROUTE = {"/users/**"};
+    public static final String[] PUBLIC_GET_ROUTE = {};
     public static final String[] PUBLIC_POST_ROUTE = {"/auth/**"};
 }
